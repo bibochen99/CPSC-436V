a9ad0b31b9f92a35b6a0e6661f8bf7f6d9e72ef7
Roy <zhongroy189@gmail.com>
2022-03-13 12:16:53 -0700

    convert data type

--- js/main.js
+++ js/main.js
@@ -11,6 +11,16 @@ Promise.all([
   data = dataset[1];
   lanLonData = dataset[2];
   data.forEach((d) => {
+    d["year"] = +d["year"];
+    d["Life Ladder"] = +d["Life Ladder"];
+    d["Log GDP per capita"] = +d["Log GDP per capita"];
+    d["Social support"] = +d["Social support"];
+    d["Healthy life expectancy at birth"] = +d["Healthy life expectancy at birth"];
+    d["Freedom to make life choices"] = +d["Freedom to make life choices"];
+    d["Generosity"] = +d["Generosity"];
+    d["Perceptions of corruption"] = +d["Perceptions of corruption"];
+    d["Positive affect"] = +d["Positive affect"];
+    d["Negative affect"] = +d["Negative affect"];
     lanLonData.forEach((data) => {
       if (d["Country name"] == data["country"]) d["lat"] = data["latitude"];
       d["lon"] = data["longitude"];
@@ -20,4 +30,5 @@ Promise.all([
   data = data.filter((d) => {
     return d.lat !== undefined && d.lon !== undefined;
   });
+
 });
