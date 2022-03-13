d8bec9c87a1b8d0f56f045894068847ae4da41f2
Roy <zhongroy189@gmail.com>
2022-03-13 15:15:36 -0700

    static map view with tooltip

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 /**
  * Load and combine data
  */
-let geoData, data, lanLonData;
+let geoData, data, lanLonData, choroplethMap;
 Promise.all([
-  d3.json("data/countries-10m.json"),
+  d3.json("data/world_countries_topo.json"),
   d3.csv("data/world-happiness-report.csv"),
   d3.csv("data/world_country_and_usa_states_latitude_and_longitude_values.csv"),
 ]).then((dataset) => {
@@ -15,7 +15,8 @@ Promise.all([
     d["Life Ladder"] = +d["Life Ladder"];
     d["Log GDP per capita"] = +d["Log GDP per capita"];
     d["Social support"] = +d["Social support"];
-    d["Healthy life expectancy at birth"] = +d["Healthy life expectancy at birth"];
+    d["Healthy life expectancy at birth"] =
+      +d["Healthy life expectancy at birth"];
     d["Freedom to make life choices"] = +d["Freedom to make life choices"];
     d["Generosity"] = +d["Generosity"];
     d["Perceptions of corruption"] = +d["Perceptions of corruption"];
@@ -30,5 +31,25 @@ Promise.all([
   data = data.filter((d) => {
     return d.lat !== undefined && d.lon !== undefined;
   });
+  // console.log(geoData);
+  // Combine both datasets by adding the population density to the TopoJSON file
+  geoData.objects.world_countries.geometries.forEach((d) => {
+    for (let i = 0; i < data.length; i++) {
+      if (d.properties.name == data[i]["Country name"]) {
+        if (data[i].year === 2013) {
+          d.properties.lifeLadder = +data[i]["Life Ladder"];
+        }
+      }
+    }
 
+  });
+  console.log(geoData);
+  // choroplethMap init
+  choroplethMap = new ChoroplethMap(
+    {
+      parentElement: "#map",
+    },
+    geoData,
+    data
+  );
 });
