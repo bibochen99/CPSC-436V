f01a1efba5fcaac19963db5e54c9b390a7ec530c
Roy <zhongroy189@gmail.com>
2022-03-17 00:20:17 -0700

    add legend, modifies data input in main js

--- js/main.js
+++ js/main.js
@@ -31,26 +31,81 @@ Promise.all([
   data = data.filter((d) => {
     return d.lat !== undefined && d.lon !== undefined;
   });
-  // console.log(geoData);
-  // Combine both datasets by adding the population density to the TopoJSON file
+
+  // Combine both datasets to the TopoJSON file
+  let inputYear = 2013;
+  let usa, RepublicofSerbia, Macedonia, PapuaNewGuinea, Myanmar;
+  let min = 100,
+    max = -100;
   geoData.objects.world_countries.geometries.forEach((d) => {
     for (let i = 0; i < data.length; i++) {
+      if (
+        data[i]["Country name"] === "United States" &&
+        data[i].year === inputYear
+      ) {
+        usa = data[i]["Life Ladder"];
+      }
+      if (data[i]["Country name"] === "Serbia" && data[i].year === inputYear) {
+        RepublicofSerbia = data[i]["Life Ladder"];
+      }
+
+      if (
+        data[i]["Country name"] === "North Macedonia" &&
+        data[i].year === inputYear
+      ) {
+        Macedonia = data[i]["Life Ladder"];
+      }
+      if (
+        data[i]["Country name"] === "Papua New Guinea" &&
+        data[i].year === inputYear
+      ) {
+        PapuaNewGuinea = data[i]["Life Ladder"];
+      }
+      if (data[i]["Country name"] === "Myanmar" && data[i].year === inputYear) {
+        Myanmar = data[i]["Life Ladder"];
+      }
+
       if (d.properties.name == data[i]["Country name"]) {
-        if (data[i].year === 2013) {
-          d.properties.year =2013;
+        if (data[i].year === inputYear) {
+          d.properties.year = inputYear;
           d.properties.lifeLadder = data[i]["Life Ladder"];
         }
       }
     }
 
+    // update name
+    if (d.properties.name == "USA") {
+      d.properties.year = inputYear;
+      d.properties.lifeLadder = usa;
+    }
+    if (d.properties.name == "Republic of Serbia") {
+      d.properties.year = inputYear;
+      d.properties.lifeLadder = RepublicofSerbia;
+    }
+    if (d.properties.name == "Macedonia") {
+      d.properties.year = inputYear;
+      d.properties.lifeLadder = Macedonia;
+    }
+    if (d.properties.name == "Guinea") {
+      d.properties.year = inputYear;
+      d.properties.lifeLadder = PapuaNewGuinea;
+    }
+    if (d.properties.name == "Myanmar") {
+      d.properties.year = inputYear;
+      d.properties.lifeLadder = Myanmar;
+    }
   });
-  console.log(geoData);
+
+  let entryData = data.filter((d) => {
+    return (d.year == 2013);
+  });
+  
   // choroplethMap init
   choroplethMap = new ChoroplethMap(
     {
       parentElement: "#map",
     },
     geoData,
-    data
+    entryData
   );
 });
