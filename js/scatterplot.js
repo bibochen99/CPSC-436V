3c432253c0a8346faf5ad61343d18903ac57ae14
UBC Student <rzhong01@students.cs.ubc.ca>
2022-04-11 21:49:52 -0700

    Update scatterplot.js

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -138,7 +138,7 @@ class Scatterplot {
       .attr('cx', d => vis.xScale(vis.xValue(d)))
       .attr('stroke', 'blue')
       .attr('fill', 'lightblue')
-      .attr('opacity',0.3)
+      .attr('opacity',0.6)
       .classed("selected", d => {
         if (d.display == true) {
           return true;
